b03b51bce2663e523d0fd79568cce06998bcb198
Erick <egmartinez@fi.uba.ar>
2024-04-22 19:30:02 -0300

    init client (wip)

---
+++ client/client.py
@@ -0,0 +1,51 @@
+import logging
+from shared.connection_handler import ConnectionHandler
+import socket
+
+class Client:
+    def __init__(self, server_ip, server_port, reviews_file, books_file, batch_size):
+        self._server_ip = server_ip
+        self._server_port = server_port
+        self._reviews_file = reviews_file
+        self._books_file = books_file
+        self._batch_size = batch_size
+        self._connection_handler = None
+        
+    def start(self):
+        logging.info("Starting client")
+        #init connection with server using ip and port, using tcp socket
+        try:
+            server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            server_socket.connect((self._server_ip, self._server_port))           
+            self._connection_handler = ConnectionHandler(server_socket)
+            logging.info("Connected to server at {}:{}".format(self._server_ip, self._server_port))
+            self.send_file_data(self._books_file)
+            self.send_file_data(self._reviews_file)        
+        except Exception as e:
+            logging.error("Failed to connect to server: {}".format(str(e)))
+            
+    def send_file_data(self, file):
+        """
+        Sends the file data to the server
+        """
+        logging.info(f"Sending data to server. File: {file}")
+        with open(file, 'r') as file:
+            # read the data from the csv file, start sending from the second line, sending in batches of configurable size and awaiting confirmation from the server before sending the next batch. This should read line by line, since we don't have infinite memory.
+        
+            completed = False
+            while not completed:
+                batch = ""
+                for i in range(self._batch_size):
+                    line = file.readline()
+                    batch += line + "\n"
+                    if not line:
+                       completed = True
+                       break
+                self._connection_handler.send_message(batch)   
+                response = self._connection_handler.read_message()
+                if response != "OK":
+                    logging.error("Server response: {}".format(response))
+                    break  
+        logging.info(f"Finish data sent to server. File: {file}")
+
+        
---
+++ client/main.py
@@ -0,0 +1,51 @@
+import logging
+from configparser import ConfigParser
+import os
+from client.client import Client
+
+def initialize_config():
+    config = ConfigParser()
+    config.read('config.ini')
+    config_params = {}
+    try:    
+        config_params['server_ip'] = os.getenv('SERVER_IP', config["DEFAULT"]["SERVER_IP"])
+        config_params['server_port'] = int(os.getenv('SERVER_PORT', config["DEFAULT"]["SERVER_PORT"]))
+        config_params['logging_level'] = os.getenv('LOGGING_LEVEL', config["DEFAULT"]["LOGGING_LEVEL"])
+        config_params['reviews_file'] = os.getenv('REVIEWS_FILE', config["DEFAULT"]["REVIEWS_FILE"])
+        config_params['books_file'] = os.getenv('BOOKS_FILE', config["DEFAULT"]["BOOKS_FILE"])    
+    except KeyError as e:
+        raise KeyError("Key was not found. Error: {}. Aborting client".format(e))
+    except ValueError as e:
+        raise ValueError("Key could not be parsed. Error: {}. Aborting client".format(e))
+
+    return config_params
+
+def initialize_log(logging_level):
+    """
+    Python custom logging initialization
+
+    Current timestamp is added to be able to identify in docker
+    compose logs the date when the log has arrived
+    """
+    logging.basicConfig(
+        format='%(asctime)s %(levelname)-8s %(message)s',
+        level=logging_level,
+        datefmt='%Y-%m-%d %H:%M:%S',
+    )
+
+def main():
+    logging.info("Starting client")
+    config_params = initialize_config()
+    logging_level = config_params["logging_level"]
+    server_ip = config_params["server_ip"]
+    server_port = config_params["server_port"]
+    reviews_file = config_params["reviews_file"]
+    books_file = config_params["books_file"]
+
+    initialize_log(logging_level)
+    client = Client(server_ip, server_port, reviews_file, books_file)
+    client.start()
+    
+
+if __name__ == "__main__":
+    main()
