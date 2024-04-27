1f32fdce36231605d0ac56e02cb44a139b6d522c
Erick <egmartinez@fi.uba.ar>
2024-04-27 11:35:24 -0300

    counter of author decades and fix expander

--- author_expander/expander.py
+++ author_expander/expander.py
@@ -19,7 +19,7 @@ class AuthorExpander:
                                                                 input_exchange_name=self.input_exchange,
                                                                 input_queues_to_recv_from=[self.input_queue_of_books]
                                                                 )
-            logging.debug(f"Create a MQConnectionHandler object for the author expander with the following parameters: output_exchange_name={self.output_exchange}, output_queues_to_bind={self.output_queues}, input_exchange_name={self.input_exchange}, input_queues_to_recv_from={self.input_queue_of_books}")
+            logging.info(f"Create a MQConnectionHandler object for the author expander with the following parameters: output_exchange_name={self.output_exchange}, output_queues_to_bind={self.output_queues}, input_exchange_name={self.input_exchange}, input_queues_to_recv_from={self.input_queue_of_books}")
         except Exception as e:
             logging.error(f"Error starting author expander: {str(e)}")
             
@@ -34,18 +34,21 @@ class AuthorExpander:
         The body is a csv batch with the following format in a line: "['author_1',...,'author_n'], decade" 
         The expansion should create multiple lines, one for each author, with the following format: "author_i, decade"
         """
-        if body == "EOF":
+        msg = body.decode()
+        if msg == "EOF":
             ch.basic_ack(delivery_tag=method.delivery_tag)
             for queue_name in self.output_queues:
                 self.mq_connection_handler.send_message(queue_name, "EOF")
+                logging.info(f"Sent EOF message to output queue {queue_name}")
             return
-        for line in body.decode().split("\n"):
+        for line in msg.split("\n"):
             if line:
                 authors, decade = eval(line)
                 for author in authors:
                     output_msg = ""
                     output_msg += author +','+ str(decade)
                     self.mq_connection_handler.send_message(self.__select_queue(author), output_msg)
+                    logging.info(f"Sent message to output queue: {output_msg}")
                     
         ch.basic_ack(delivery_tag=method.delivery_tag)
         
--- author_expander/test.py
+++
@@ -1,8 +0,0 @@
-line = "['author_1', 'author_n'],1960 \n"
-authors, decade = eval(line)
-expanded_authors = []
-print(authors)
-print(decade)
-for author in authors:
-    expanded_authors += author +','+ str(decade) + '\n'
-print(str(expanded_authors))
