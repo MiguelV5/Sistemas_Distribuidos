5c1ea47d5b5dcd4d76bb69b23b6e9b2fceafd069
MiguelV5 <mvasquezj@fi.uba.ar>
2024-04-28 03:44:00 -0300

    finish preprocessors

--- decade_preprocessor/decade_preprocessor.py
+++ decade_preprocessor/decade_preprocessor.py
@@ -1,13 +1,27 @@
+import io
 from shared.mq_connection_handler import MQConnectionHandler
 import signal
 import logging
+import csv
+from shared import constants
+
+
+TITLE_IDX = 0
+AUTHORS_IDX = 1
+YEAR_IDX = 2
+CATEGORIES_IDX = 3
 
 class DecadePreprocessor:
-    def __init__(self, input_exchange: str, input_queue: str, output_exchange: str, output_queue_towards_preproc: str, output_queue_towards_filter: str):
-        self.output_queue = output_queue_towards_preproc
+    def __init__(self, input_exchange: str, input_queue: str, output_exchange: str, output_queue_towards_expander: str, output_queues_towards_mergers: list[str]):
+        self.output_queue_towards_expander = output_queue_towards_expander
+        self.output_queues_towards_mergers = output_queues_towards_mergers
+        
+        output_queues_to_bind = {output_queue_towards_expander: [output_queue_towards_expander]}
+        for output_queue_towards_merger in output_queues_towards_mergers:
+            output_queues_to_bind[output_queue_towards_merger] = [output_queue_towards_merger]
+
         self.mq_connection_handler = MQConnectionHandler(output_exchange, 
-                                                         {output_queue_towards_preproc: [output_queue_towards_preproc],
-                                                          output_queue_towards_filter: [output_queue_towards_filter]},
+                                                         output_queues_to_bind,
                                                          input_exchange,
                                                          [input_queue])
         
@@ -20,11 +34,48 @@ class DecadePreprocessor:
 
 
     def __preprocess_batch(self, ch, method, properties, body):
-        # TODO
-        pass
+        msg = body.decode()
+        if msg == constants.FINISH_MSG:
+            for output_queue in self.output_queues_towards_mergers:
+                self.mq_connection_handler.send_message(output_queue, msg)
+            self.mq_connection_handler.send_message(self.output_queue_towards_expander, msg)
+            ch.basic_ack(delivery_tag=method.delivery_tag)
+            self.mq_connection_handler.close_connection()
+        else:
+            batch = csv.reader(io.StringIO(msg), delimiter=',', quotechar='"')
+            batch_to_send_towards_expander = ""
+            batches_to_send_towards_mergers = {output_queue: "" for output_queue in self.output_queues_towards_mergers}
+            for row in batch:
+                title = row[TITLE_IDX]
+                authors = row[AUTHORS_IDX]
+                year = row[YEAR_IDX]
+                categories = row[CATEGORIES_IDX]
+                decade = self.__extract_decade(year)
+
+                batch_to_send_towards_expander += f"{authors},{decade}" + "\n"
+                selected_merger_queue = self.__select_merger_queue(title)
+                batches_to_send_towards_mergers[selected_merger_queue] += f"{title},{authors},{categories},{decade}" + "\n"
+            
+            self.mq_connection_handler.send_message(self.output_queue_towards_expander, batch_to_send_towards_expander)
+            for output_queue in self.output_queues_towards_mergers:
+                self.mq_connection_handler.send_message(output_queue, batches_to_send_towards_mergers[output_queue])
+
+            ch.basic_ack(delivery_tag=method.delivery_tag)
+
+    def __extract_decade(self, year: str) -> int:
+        year = int(year)
+        decade = year - (year % 10)
+        return decade
 
 
-
+    def __select_merger_queue(self, title: str) -> str:
+        """
+        Should return the queue name where the review should be sent to.
+        It uses the hash of the title to select a queue on self.output_queue_towards_mergers
+        """
+        hash_value = hash(title)
+        queue_index = hash_value % len(self.output_queues_towards_mergers)
+        return self.output_queues_towards_mergers[queue_index]
 
     def start(self):
         self.mq_connection_handler.start_consuming()
---
+++ year_preprocessor/main.py
@@ -0,0 +1,19 @@
+import logging
+from year_preprocessor import YearPreprocessor
+from shared.initializers import init_log, init_configs
+
+def main():
+    config_params = init_configs(["LOGGING_LEVEL", "INPUT_EXCHANGE", "OUTPUT_EXCHANGE", "INPUT_QUEUE_OF_BOOKS", "OUTPUT_QUEUE_OF_BOOKS_TOWARDS_PREPROC", "OUTPUT_QUEUE_OF_BOOKS_TOWARDS_FILTER"])
+    init_log(config_params["LOGGING_LEVEL"])
+
+    logging.info("Starting decade_preprocessor")
+    decade_preprocessor = YearPreprocessor(input_exchange=config_params["INPUT_EXCHANGE"], 
+                                           input_queue=config_params["INPUT_QUEUE_OF_BOOKS"],
+                                           output_exchange=config_params["OUTPUT_EXCHANGE"],
+                                           output_queue_towards_preproc=config_params["OUTPUT_QUEUE_OF_BOOKS_TOWARDS_PREPROC"],
+                                           output_queue_towards_filter=config_params["OUTPUT_QUEUE_OF_BOOKS_TOWARDS_FILTER"])
+    decade_preprocessor.start()
+
+
+if __name__ == "__main__":
+    main()
