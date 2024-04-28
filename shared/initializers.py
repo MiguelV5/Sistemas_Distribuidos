48503551c491765b9e5fec2ed87e7ee212174205
MiguelV5 <mvasquezj@fi.uba.ar>
2024-04-28 03:42:14 -0300

    reduce pika logging verbosity to critical only

--- shared/initializers.py
+++ shared/initializers.py
@@ -9,6 +9,7 @@ def init_log(logging_level: str):
     Current timestamp is added to be able to identify in docker
     compose logs the date when the log has arrived
     """
+    logging.getLogger("pika").setLevel(logging.WARNING)
     logging.basicConfig(
         format='%(asctime)s %(levelname)-8s %(message)s',
         level=logging_level,
