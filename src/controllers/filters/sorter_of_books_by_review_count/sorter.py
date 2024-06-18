8d62e58682307db146fe04dd65dffc639dc46ba3
Erick <egmartinez@fi.uba.ar>
2024-06-18 20:42:58 -0300

    q4 updated with dup detection and system msg

--- src/controllers/filters/sorter_of_books_by_review_count/sorter.py
+++ src/controllers/filters/sorter_of_books_by_review_count/sorter.py
@@ -4,6 +4,7 @@ import csv
 import io
 import logging
 from shared.monitorable_process import MonitorableProcess
+from shared.protocol_messages import SystemMessage, SystemMessageType
 
 TITLE_IDX = 0
 SCORES_IDX = 1
@@ -22,38 +23,38 @@ class Sorter(MonitorableProcess):
         self.input_queue = input_queue
         self.output_queue = output_queue
         self.required_top_of_books = required_top_of_books
-        self.best_books = []
         self.mq_connection_handler = MQConnectionHandler(output_exchange_name=self.output_exchange, 
                                                          output_queues_to_bind={self.output_queue: [self.output_queue]},
                                                          input_exchange_name=self.input_exchange,
                                                          input_queues_to_recv_from=[self.input_queue])
         
     def start(self):
-        self.mq_connection_handler.setup_callbacks_for_input_queue(self.input_queue, self.__sort_books)
+        self.mq_connection_handler.setup_callbacks_for_input_queue(self.input_queue, self.state_handler_callback, self.__sort_books)
         self.mq_connection_handler.start_consuming()
         
-    def __sort_books(self, ch, method, properties, body):
+    def __sort_books(self, body: SystemMessage):
         """
         The body is a csv line with the following format in the line: "title,[scores]"
         """
-        msg = body.decode()
-        if msg == constants.FINISH_MSG:
-            self.mq_connection_handler.send_message(self.output_queue, f"\"{self.best_books}\"")
-            self.mq_connection_handler.send_message(self.output_queue, constants.FINISH_MSG)
-            self.best_books = []
-            ch.basic_ack(delivery_tag=method.delivery_tag)
+        msg = body.payload
+        if body.type == SystemMessageType.EOF_R:
+            next_seq_num = self.get_seq_num_to_send(body.client_id, self.controller_name)
+            self.mq_connection_handler.send_message(self.output_queue, SystemMessage(SystemMessageType.DATA, body.client_id, self.controller_name, next_seq_num, f"\"{self.state[body.client_id]["best_books"]}\"").encode_to_str())
+            self.update_self_seq_number(body.client_id, next_seq_num)
+            next_seq_num = self.get_seq_num_to_send(body.client_id, self.controller_name)
+            self.mq_connection_handler.send_message(self.output_queue, SystemMessage(SystemMessageType.EOF_R, body.client_id, self.controller_name, next_seq_num).encode_to_str())
+            self.state[body.client_id]["best_books"] = []
         else:
             book = csv.reader(io.StringIO(msg), delimiter=',', quotechar='"')
             for row in book:
                 scores = eval(row[SCORES_IDX])
                 scores = list(map(int, scores))
                 avg_score = sum(scores) / len(scores)
-                if len(self.best_books) < self.required_top_of_books:
-                    self.best_books.append((row[TITLE_IDX], avg_score))
-                    self.best_books.sort(key=lambda x: x[SCORES_IDX], reverse=True)
+                if len(self.state[body.client_id].get("best_books",[])) < self.required_top_of_books:
+                    self.state[body.client_id].get("best_books",[]).append((row[TITLE_IDX], avg_score))
+                    self.state[body.client_id].get("best_books",[]).sort(key=lambda x: x[SCORES_IDX], reverse=True)
                 else:
-                    if avg_score > self.best_books[-1][SCORES_IDX]:
-                        self.best_books.append((row[TITLE_IDX], avg_score))
-                        self.best_books.sort(key=lambda x: x[SCORES_IDX], reverse=True)
-                        self.best_books.pop()
-            ch.basic_ack(delivery_tag=method.delivery_tag)
+                    if avg_score > self.state[body.client_id].get("best_books",[])[-1][SCORES_IDX]:
+                        self.state[body.client_id].get("best_books",[]).append((row[TITLE_IDX], avg_score))
+                        self.state[body.client_id].get("best_books",[]).sort(key=lambda x: x[SCORES_IDX], reverse=True)
+                        self.state[body.client_id].get("best_books",[]).pop()
--- src/controllers/sinks/query4_result_generator/generator.py
+++ src/controllers/sinks/query4_result_generator/generator.py
@@ -2,6 +2,7 @@ from shared.mq_connection_handler import MQConnectionHandler
 import logging
 from shared import constants
 from shared.monitorable_process import MonitorableProcess
+from shared.protocol_messages import SystemMessage, SystemMessageType
 
 TITLE_IDX = 0
 SCORES_IDX = 1
@@ -25,23 +26,25 @@ class Generator(MonitorableProcess):
                                                          input_queues_to_recv_from=[self.input_queue])
         
     def start(self):
-        self.mq_connection_handler.setup_callbacks_for_input_queue(self.input_queue, self.__generate)
+        self.mq_connection_handler.setup_callbacks_for_input_queue(self.input_queue, self.state_handler_callback, self.__generate)
         self.mq_connection_handler.start_consuming()
         
-    def __generate(self, ch, method, properties, body):
+    def __generate(self, body: SystemMessage):
         """
         The body is a csv list with the following: "[(title,review_score)]" 
         The generator should send the result to the output queue.
         """
-        msg = body.decode()
-        if msg == constants.FINISH_MSG:
-            ch.basic_ack(delivery_tag=method.delivery_tag)
-            self.output_msg = "[Q4 Results]:  (Title, ReviewScore)"
+        msg = body.payload
+        if body.type == SystemMessageType.EOF_R:
+            next_seq_num = self.get_seq_num_to_send(body.client_id, self.controller_name)
+            self.mq_connection_handler.send_message(self.output_queue, SystemMessage(SystemMessageType.EOF_R, body.client_id, self.controller_name, next_seq_num).encode_to_str())
+            self.update_self_seq_number(body.client_id, next_seq_num)
         else:
             books = eval(msg.replace("\"",""))
             for book in books:
                 self.output_msg += "\n" f"{book[TITLE_IDX]},{book[SCORES_IDX]}"
             logging.info("Sending Q4 results to output queue")
-            self.mq_connection_handler.send_message(self.output_queue, self.output_msg)
-            ch.basic_ack(delivery_tag=method.delivery_tag)
+            next_seq_num = self.get_seq_num_to_send(body.client_id, self.controller_name)
+            self.mq_connection_handler.send_message(self.output_queue, SystemMessage(SystemMessageType.DATA, body.client_id, self.controller_name, next_seq_num, self.output_msg).encode_to_str())
+            self.update_self_seq_number(body.client_id, next_seq_num)
 
